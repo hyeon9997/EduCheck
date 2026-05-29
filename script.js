1d82642e72b6e9a87a2380a6b292e63a5a3a57ac
hyeon9997 <62966946+hyeon9997@users.noreply.github.com>
2026-05-29 18:53:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -267,9 +267,9 @@ function checkGroupCredit_minor(selectedSubjects) {
   });
 
   if (selected_total >= 16) {
-    return `✅ 기술·가정/정보, 제2외국어/한문, 교양 교과(군) 학점 ${selected_total}/16 충족<br>`;
+    return `✅ 기술·가정/정보, 제2외국어/한문, 교양 교과(군) 학점 충족<br>`;
   } else {
-    return `❌ 기술·가정/정보, 제2외국어/한문, 교양 교과(군) 학점 ${selected_total}/16 부족<br>`;
+    return `❌ 기술·가정/정보, 제2외국어/한문, 교양 교과(군) ${9-selected_total}학점 부족<br>`;
   }
 }
 
