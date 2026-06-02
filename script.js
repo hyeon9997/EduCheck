358ca2a1a08fd1a9b53e4a72230f728747d164b8
hyeon9997 <62966946+hyeon9997@users.noreply.github.com>
2026-06-02 20:11:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 const subjects = [
   { id: "S1", grade: 2, semester: 1, group: "수학", type:"진로", name: "인공지능 수학", credit: 3 },
   { id: "S2", grade: 2, semester: 1, group: "체육", type:"진로", name: "기초 체육 전공 실기", credit: 3 },
-  { id: "S3", grade: 2, semester: 1, group: "기술·가정/정보", type:"일반", name: "기술·가정", credit: 3 },  
-  { id: "S4", grade: 2, semester: 1, group: "기술·가정/정보", type:"진로", name: "인공지능과 피지컬 컴퓨팅", credit: 3 },
-  { id: "S5", grade: 2, semester: 1, group: "기술·가정/정보", type:"진로", name: "데이터 과학", credit: 3 },
+  { id: "S3", grade: 2, semester: 1, group: "기술·가정/ 정보", type:"일반", name: "기술·가정", credit: 3 },  
+  { id: "S4", grade: 2, semester: 1, group: "기술·가정/ 정보", type:"진로", name: "인공지능과 피지컬 컴퓨팅", credit: 3 },
+  { id: "S5", grade: 2, semester: 1, group: "기술·가정/ 정보", type:"진로", name: "데이터 과학", credit: 3 },
   { id: "S6", grade: 2, semester: 1, group: "제2외국어/한문", type:"일반", name: "중국어", credit: 3 },
   { id: "S7", grade: 2, semester: 1, group: "제2외국어/한문", type:"일반", name: "일본어", credit: 3 },
   { id: "S8", grade: 2, semester: 1, group: "사회(역사/도덕 포함)", type:"일반", name: "세계사", credit: 3 },
@@ -20,9 +20,9 @@ const subjects = [
   
   { id: "S16", grade: 2, semester: 2, group: "수학", type:"진로", name: "기하", credit: 3 },
   { id: "S17", grade: 2, semester: 2, group: "체육", type:"진로", name: "심화 체육 전공 실기", credit: 3 },  
-  { id: "S18", grade: 2, semester: 2, group: "기술·가정/정보", type:"융합", name: "소프트웨어와 생활", credit: 3 },  
-  { id: "S19", grade: 2, semester: 2, group: "기술·가정/정보", type:"진로", name: "인공지능 기초", credit: 3 },  
-  { id: "S20", grade: 2, semester: 2, group: "기술·가정/정보", type:"융합", name: "창의 공학 설계", credit: 3 },  
+  { id: "S18", grade: 2, semester: 2, group: "기술·가정/ 정보", type:"융합", name: "소프트웨어와 생활", credit: 3 },  
+  { id: "S19", grade: 2, semester: 2, group: "기술·가정/ 정보", type:"진로", name: "인공지능 기초", credit: 3 },  
+  { id: "S20", grade: 2, semester: 2, group: "기술·가정/ 정보", type:"융합", name: "창의 공학 설계", credit: 3 },  
   { id: "S21", grade: 2, semester: 2, group: "제2외국어/한문", type:"융합", name: "중국 문화", credit: 3 },  
   { id: "S22", grade: 2, semester: 2, group: "제2외국어/한문", type:"융합", name: "일본 문화", credit: 3 },  
   { id: "S23", grade: 2, semester: 2, group: "사회(역사/도덕 포함)", type:"진로", name: "동아시아 역사 기행", credit: 3 },  
@@ -37,7 +37,7 @@ const subjects = [
   { id: "S31", grade: 3, semester: 1, group: "국어", type:"진로", name: "주제 탐구 독서", credit: 3 },  
   { id: "S32", grade: 3, semester: 1, group: "수학", type:"진로", name: "미적분Ⅱ", credit: 3 },  
   { id: "S33", grade: 3, semester: 1, group: "영어", type:"융합", name: "미디어 영어", credit: 3 },  
-  { id: "S34", grade: 3, semester: 1, group: "기술·가정/정보", type:"진로", name: "정보과학", credit: 3 },  
+  { id: "S34", grade: 3, semester: 1, group: "기술·가정/ 정보", type:"진로", name: "정보과학", credit: 3 },  
   { id: "S35", grade: 3, semester: 1, group: "사회(역사/도덕 포함)", type:"일반", name: "세계시민과 지리", credit: 3 },  
   { id: "S36", grade: 3, semester: 1, group: "사회(역사/도덕 포함)", type:"진로", name: "경제", credit: 3 },  
   { id: "S37", grade: 3, semester: 1, group: "사회(역사/도덕 포함)", type:"진로", name: "윤리와 사상", credit: 3 },  
@@ -59,7 +59,7 @@ const subjects = [
   { id: "S52", grade: 3, semester: 2, group: "과학", type:"융합", name: "과학의 역사와 문화", credit: 3 }, 
   { id: "S53", grade: 3, semester: 2, group: "과학", type:"융합", name: "기후변화와 환경생태", credit: 3 }, 
   { id: "S54", grade: 3, semester: 2, group: "과학", type:"융합", name: "융합과학 탐구", credit: 3 }, 
-  { id: "S55", grade: 3, semester: 2, group: "기술·가정/정보", type:"진로", name: "사물인터넷", credit: 3 }, 
+  { id: "S55", grade: 3, semester: 2, group: "기술·가정/ 정보", type:"진로", name: "사물인터넷", credit: 3 }, 
 
 ];
 
@@ -84,7 +84,7 @@ const prerequisites = {
   "행성우주과학": ["지구과학"],
 };
 
-const targetGroups = ["기술·가정/정보", "제2외국어/한문", "교양"];
+const targetGroups = ["기술·가정/ 정보", "제2외국어/한문", "교양"];
 const minGroupCredit = 10; //정보, 한문 1학기 필수 이수
 
 // =======================
@@ -259,7 +259,7 @@ function checkSelectionCount(selectedSubjects) {
 }
 
 function checkGroupCredit_minor(selectedSubjects) {
-  const targetGroups = ["기술·가정/정보", "제2외국어/한문", "교양"];
+  const targetGroups = ["기술·가정/ 정보", "제2외국어/한문", "교양"];
 
   let selected_total = 6;
 
@@ -270,9 +270,9 @@ function checkGroupCredit_minor(selectedSubjects) {
   });
 
   if (selected_total >= 16) {
-    return `✅ 기술·가정/정보, 제2외국어/한문, 교양 교과(군) 학점 충족<br>`;
+    return `✅ 기술·가정/ 정보, 제2외국어/한문, 교양 교과(군) 학점 충족<br>`;
   } else {
-    return `❌ 기술·가정/정보, 제2외국어/한문, 교양 교과(군) ${16-selected_total}학점 부족<br>`;
+    return `❌ 기술·가정/ 정보, 제2외국어/한문, 교양 교과(군) ${16-selected_total}학점 부족<br>`;
   }
 }
 
